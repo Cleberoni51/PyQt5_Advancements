e7d0af62fdb98400357ccfd7558a04db6026556d
Cleberoni51 <caleb.schear@gmail.com>
2025-06-11 20:40:48 -0400

    optimized the window settings

--- PyQt5_Advancements_1.1.py
+++ PyQt5_Advancements_1.1.py
@@ -6,24 +6,26 @@
 import sys
 import PyQt5.QtWidgets as qtw
 import PyQt5.QtGui as qtg
+import PyQt5.QtCore as qtc
 
 class MainWindow(qtw.QWidget):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, parent=None):
+        super(MainWindow, self).__init__(parent)
+        self.resize(1000,250)
+        self.setWindowTitle("PyQt5")
+        self.label = qtw.QLabel(self)
+        self.label.setText("Hello World")
+        font = qtg.QFont()
+        font.setFamily("Arial")
+        font.setPointSize(16)
+        self.label.setFont(font)
+        self.label.move(50,20)
 
-        #Add a Title for your Widget
-        self.setWindowTitle("Name Maker 10000")
+def main():
+    app = qtw.QApplication(sys.argv)
+    ex = MainWindow()
+    ex.show()
+    sys.exit(app.exec_())
 
-
-
-
-
-
-
-
-        self.show()
-
-app = qtw.QApplication([])
-mw = MainWindow()
-
-app.exec_()
+if __name__ == '__main__':
+    main()
